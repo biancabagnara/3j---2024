7ee5ebcd09cac02d2f012f991d9c230daf3bd244
biancabagnara <108936801+biancabagnara@users.noreply.github.com>
2024-06-14 09:42:16 -0300

    14-06-2024

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 const botoes = document.querySelectorAll(".botao");
 const conteudo = document.querySelectorAll(".aba-conteudo");
 const tempo = document.querySelectorAll(".contador");
+tempo[0].textContent = "Bibs"
+const tempoObjetiv1 = new Date(2024,11,31,59,59);
+const tempoObjetivo2 = new Date(2024,11,31,59,59);
+const tempoObjetivo3 = new Date(2024,11,31,59,59);
+const tempoObjetivo4 = new Date(2024,11,31,59,59);
+const agora = new Date();
+tempo[1].textContent = agora
 for(let i = 0; i < botoes.length ; i++){
     botoes[i].onclick = function(){
         for (let j = 0 ; j < botoes.length ; j++){
