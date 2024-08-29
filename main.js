a0ee2100c61cd79e68b4e078082e919e3e761423
biancabagnara <108936801+biancabagnara@users.noreply.github.com>
2024-08-29 08:07:33 -0300

    29-08

--- main.js
+++ main.js
@@ -1,44 +1,47 @@
-
-const botoes = document.querySelectorAll(".botao");
-const conteudo = document.querySelectorAll(".aba-conteudo");
+const botoes  = document.querySelectorAll(".botao");
+const conteudo = document.querySelectorAll(".abas-conteudo");
 const tempo = document.querySelectorAll(".contador");
-tempo[0].textContent = "Bibs"
-const tempoObjetivo1 = new Date(2024,11,31,59,59);
-const tempoObjetivo2 = new Date(2024,11,31,59,59);
-const tempoObjetivo3 = new Date(2024,11,31,59,59);
-const tempoObjetivo4 = new Date(2024,11,31,59,59);
+const tempoObjetivo1 = new Date(2024,11,24,23,59);
+const tempoObjetivo2 = new Date(2024,9,28,23,59);
+const tempoObjetivo3 = new Date(2025,0,1,23,59);
+const tempoObjetivo4 = new Date(2025,2,14,23,59);
 
 function calculaTempo(tempoObjetivo){
-const agora = new Date();
-let segundos;
-let minuto;
-let horas;
-let dias;
+    const agora = new Date ()
+    let segundos
+    let minutos
+    let horas
+    let dias
+ segundos = Math.floor((tempoObjetivo-agora)/1000)
+ minutos = Math.floor(segundos/60)
+ horas = Math.floor(minutos/60)
+ dias = Math.floor(horas/24)
 
-segundos = Math.floor((tempoObjetivo-agora)/1000);
-minutos = Math.floor(segundos/60);
-horas = Math.floor(minutos/60);
-dias = Math.floor(horas/24);
+segundos = segundos%60
+minutos = minutos%60
+horas = horas%24
 
-segundos= segundos%60
-minutos= minutos %60
-horas= horas%24
 if (segundos >= 0){
-    return `Faltam ${dias} dias, ${horas} horas, ${minutos} minutos, ${segundos} segundos,`
+return [dias,horas,minutos,segundos]
 } else {
     return 'Prazo Finalizado!'
-
-  }
 }
+}
+const tempoUm = calculaTempo(tempoObjetivo1)
+const tempoDois = calculaTempo(tempoObjetivo2)
+const tempoTres = calculaTempo(tempoObjetivo3)
+const tempoQuatro = calculaTempo(tempoObjetivo4)
 
-tempo[0].textContent = calculaTempo(tempoObjetivo1);
-tempo[1].textContent = calculaTempo(tempoObjetivo2);
-tempo[2].textContent = calculaTempo(tempoObjetivo3);
-tempo[3].textContent = calculaTempo(tempoObjetivo4);
+tempoUm[3]
 
-for(let i = 0; i < botoes.length ; i++){
+//tempo[0].textContent = calculaTempo(tempoObjetivo1);
+//tempo[0].textContent = calculaTempo(tempoObjetivo2);
+//tempo[0].textContent = calculaTempo(tempoObjetivo3);
+//tempo[0].textContent = calculaTempo(tempoObjetivo4);
+
+for (let i = 0; i < botoes.length; i++) {
     botoes[i].onclick = function(){
-        for (let j = 0 ; j < botoes.length ; j++){
+        for (let j=0;j<botoes.length;j++){
             botoes[j].classList.remove("ativo");
             conteudo[j].classList.remove("ativo")
         }
